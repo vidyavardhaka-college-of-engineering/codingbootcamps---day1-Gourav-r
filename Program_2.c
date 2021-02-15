2d4c1428354553c506935268ba3c6e2e2f1de433
Gourav R <78352781+Gourav-r@users.noreply.github.com>
2021-02-15 19:22:37 +0530

    Update Program_2.c

--- Program_2.c
+++ Program_2.c
@@ -12,9 +12,9 @@ int main()
 {
 //Decleade Integer n and write scanf code bellow 
 int n;
-
-//if you write correcr code your value will print
 scanf("%d",&n);
+//if you write correcr code your value will print
+printf("%d",n);
 printf("Good Job...");
 return 0;
 }
